37bb011449db968210ea8e8d6e35ace75413774e
abdu <abdu>
2024-06-06 12:56:07 +0500

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+let ism = prompt("Ismingizni kiriting: ");
+let age = +prompt("Tugilgan yilingizni kiriting: ");
+let year = +prompt("Hozirgi yilni kiriting: ");
+function Year(c, d) {
+  return c - d;
+}console.log(`${ism} sizning yoshingiz ${Year(year, age)} da`);
